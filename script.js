8cc01c5c1f262c637b89dd25970340c549404c49
lets-unbound <139226825+lets-unbound@users.noreply.github.com>
2023-07-17 23:45:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,206 +1,195 @@
-
 const toggleButton = document.getElementById('toggleButton');
 const sectionContent = document.getElementById('sectionContent');
-const c= document.getElementById("canvas")
-const closeButton=document.getElementById("closeButton")
+const c = document.getElementById("canvas");
+const closeButton = document.getElementById("closeButton");
+const clearButton = document.getElementById("clearButton");
+const instructions = document.getElementById("instructions");
 
 toggleButton.addEventListener('click', () => {
-  if (sectionContent.style.display === 'none') {
-    sectionContent.style.display = 'block';
-    c.style.display='none'
-  } else {
+  if (instructions.style.display === 'none') {
+    instructions.style.display = 'block';
     sectionContent.style.display = 'none';
-    c.style.display="block"
+    c.style.display = 'none';
+  } else {
+    instructions.style.display = 'none';
+    sectionContent.style.display = 'block';
+    c.style.display = 'block';
   }
-
-  
 });
 
+clearButton.addEventListener('click', () => {
+  clearCanvas();
+  dots = [];
+});
 
-closeButton.addEventListener('click',()=>{
+closeButton.addEventListener('click', () => {
   sectionContent.style.display = 'none';
-  c.style.display="block"
-})
-
-
-
+  c.style.display = "block";
+});
 
 const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    let mouseX = 0;
-    let mouseY = 0;
-    let dotsMode = false;
-  
-    let dots = [];
-    let points = [];
+let mouseX = 0;
+let mouseY = 0;
+let dotsMode = false;
 
-    
+let dots = [];
+let points = [];
 
-    function activateDotsMode() {
-      patternMode = false;
-      dotsMode = true;
-      triMode = false;
-      clearCanvas();
-      alert("Hover your mouse/hover on your touch screen to see cool effects")
-    }
+function activateDotsMode() {
+  clearCanvas();
+  patternMode = false;
+  dotsMode = true;
+  triMode = false;
 
-    
+  alert("Hover your mouse/hover on your touch screen to see cool effects");
+}
 
+function handleInput(x, y) {
+  const rect = canvas.getBoundingClientRect();
+  mouseX = x - rect.left;
+  mouseY = y - rect.top;
 
-    function handleInput(x, y) {
-      const rect = canvas.getBoundingClientRect();
-      mouseX = x - rect.left;
-      mouseY = y - rect.top;
+  if (dotsMode) {
+    updateDots();
+  }
+}
 
-      
-      if (dotsMode) {
-        updateDots();
-      }
+function handleTouchEvent(e) {
+  e.preventDefault();
+  const touch = e.touches[0];
+  handleInput(touch.clientX, touch.clientY);
+}
 
-     
-    }
+function handleClickEvent(e) {
+  handleInput(e.clientX, e.clientY);
+}
 
-    function handleTouchEvent(e) {
-      e.preventDefault();
-      const touch = e.touches[0];
-      handleInput(touch.clientX, touch.clientY);
-    }
+canvas.addEventListener('mousemove', (e) => {
+  handleInput(e.clientX, e.clientY);
+});
 
-    function handleClickEvent(e) {
-      handleInput(e.clientX, e.clientY);
-    }
+canvas.addEventListener('touchmove', (e) => {
+  e.preventDefault();
+  const touch = e.touches[0];
+  handleInput(touch.clientX, touch.clientY);
+}, { passive: false });
 
-    canvas.addEventListener('mousemove', (e) => {
-      handleInput(e.clientX, e.clientY);
-    });
+canvas.addEventListener('click', (e) => {
+  handleInput(e.clientX, e.clientY);
+});
 
-    canvas.addEventListener('touchmove', (e) => {
-      e.preventDefault();
-      const touch = e.touches[0];
-      handleInput(touch.clientX, touch.clientY);
-    }, { passive: false });
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-    canvas.addEventListener('click', (e) => {
-      handleInput(e.clientX, e.clientY);
-    });
-   
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-    function clearCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
+function updateDots() {
+  const cohesionFactor = 0;
+  const separationFactor = 0.02;
+  const alignmentFactor = 0.02;
+  const maxSpeed = 2;
 
-    function getRandomColor() {
-      const letters = '0123456789ABCDEF';
-      let color = '#';
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    }
-  
-    
-    function updateDots() {
-      const cohesionFactor = 0;
-      const separationFactor = 0.02;
-      const alignmentFactor = 0.02;
-      const maxSpeed = 2;
+  dots.push({
+    x: mouseX,
+    y: mouseY,
+    vx: Math.random() * maxSpeed - maxSpeed / 2,
+    vy: Math.random() * maxSpeed - maxSpeed / 2,
+    color: getRandomColor(),
+    size: Math.floor(Math.random() * 10) + 2
+  });
 
-      dots.push({
-        x: mouseX,
-        y: mouseY,
-        vx: Math.random() * maxSpeed - maxSpeed / 2,
-        vy: Math.random() * maxSpeed - maxSpeed / 2,
-        color: getRandomColor(),
-        size: Math.floor(Math.random() * 10) + 2
-      });
+  dots.forEach((dot) => {
+    dot.x += dot.vx;
+    dot.y += dot.vy;
 
-      dots.forEach((dot) => {
-        dot.x += dot.vx;
-        dot.y += dot.vy;
+    let dx = 0;
+    let dy = 0;
+    let count = 0;
 
-        let dx = 0;
-        let dy = 0;
-        let count = 0;
+    dots.forEach((otherDot) => {
+      if (dot !== otherDot) {
+        const distance = Math.sqrt((dot.x - otherDot.x) ** 2 + (dot.y - otherDot.y) ** 2);
 
-        dots.forEach((otherDot) => {
-          if (dot !== otherDot) {
-            const distance = Math.sqrt((dot.x - otherDot.x) ** 2 + (dot.y - otherDot.y) ** 2);
-
-            if (distance < 50) {
-              dx += otherDot.x - dot.x;
-              dy += otherDot.y - dot.y;
-              count++;
-            }
-
-            if (distance < 10) {
-              dot.vx -= (otherDot.x - dot.x) * separationFactor;
-              dot.vy -= (otherDot.y - dot.y) * separationFactor;
-            }
-          }
-        });
-
-        if (count > 0) {
-          dx /= count;
-          dy /= count;
-          dot.vx += dx * cohesionFactor;
-          dot.vy += dy * cohesionFactor;
+        if (distance < 50) {
+          dx += otherDot.x - dot.x;
+          dy += otherDot.y - dot.y;
+          count++;
         }
 
-        dot.vx += Math.random() * alignmentFactor - alignmentFactor / 2;
-        dot.vy += Math.random() * alignmentFactor - alignmentFactor / 2;
-
-        const speed = Math.sqrt(dot.vx ** 2 + dot.vy ** 2);
-        if (speed > maxSpeed) {
-          dot.vx = (dot.vx / speed) * maxSpeed;
-          dot.vy = (dot.vy / speed) * maxSpeed;
+        if (distance < 10) {
+          dot.vx -= (otherDot.x - dot.x) * separationFactor;
+          dot.vy -= (otherDot.y - dot.y) * separationFactor;
         }
-
-        dot.x = Math.max(0, Math.min(dot.x, canvas.width));
-        dot.y = Math.max(0, Math.min(dot.y, canvas.height));
-      });
-
-      dots = dots.filter((dot) => dot.x > 0 && dot.x < canvas.width && dot.y > 0 && dot.y < canvas.height);
-
-      drawDots();
-    }
-
-    function drawDots() {
-      clearCanvas();
-
-      dots.forEach((dot) => {
-        ctx.beginPath();
-        ctx.arc(dot.x, dot.y, dot.size, 0, Math.PI * 2);
-        ctx.fillStyle = dot.color;
-        ctx.fill();
-        ctx.closePath();
-      });
-    }
-
-    function getRandomColor() {
-      const letters = '0123456789ABCDEF';
-      let color = '#';
-      for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
       }
-      return color;
+    });
+
+    if (count > 0) {
+      dx /= count;
+      dy /= count;
+      dot.vx += dx * cohesionFactor;
+      dot.vy += dy * cohesionFactor;
     }
 
+    dot.vx += Math.random() * alignmentFactor - alignmentFactor / 2;
+    dot.vy += Math.random() * alignmentFactor - alignmentFactor / 2;
 
-    
-
-    function clearScreen(){
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const speed = Math.sqrt(dot.vx ** 2 + dot.vy ** 2);
+    if (speed > maxSpeed) {
+      dot.vx = (dot.vx / speed) * maxSpeed;
+      dot.vy = (dot.vy / speed) * maxSpeed;
     }
 
-    function animate() {
-      requestAnimationFrame(animate);
+    dot.x = Math.max(0, Math.min(dot.x, canvas.width));
+    dot.y = Math.max(0, Math.min(dot.y, canvas.height));
+  });
 
-      // Insert any additional animations or updates here
+  dots = dots.filter((dot) => dot.x > 0 && dot.x < canvas.width && dot.y > 0 && dot.y < canvas.height);
 
-    }
+  drawDots();
+}
 
-    animate();
+function drawDots() {
+  clearCanvas();
+
+  dots.forEach((dot) => {
+    ctx.beginPath();
+    ctx.arc(dot.x, dot.y, dot.size, 0, Math.PI * 2);
+    ctx.fillStyle = dot.color;
+    ctx.fill();
+    ctx.closePath();
+  });
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function clearScreen() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Insert any additional animations or updates here
+}
+
+animate();
